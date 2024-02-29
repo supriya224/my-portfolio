e91e380400348b98418a5a39e6c2062499cbc712
supriya224 <52038704+supriya224@users.noreply.github.com>
2024-03-01 02:06:08 +0530

    Reduce image size.

--- src/Pages/ImageSection.js
+++ src/Pages/ImageSection.js
@@ -5,7 +5,7 @@ const ImageSection = () => {
     return (
         <ImageSectionStyled>
             <div className="left_content">
-                <img src="https://supriya224.github.io/my-portfolio/assets/img.jpeg"  alt="notLoad" loading='lazy' />
+                <img src="https://supriya224.github.io/my-portfolio/assets/img.jpeg" alt="notLoad" loading='lazy' />
             </div>
             <div className="right_content">
                 <div className="sub_title">
